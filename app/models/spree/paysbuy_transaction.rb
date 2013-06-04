b5e92dd4d89435fe69148bc769ed882335aeb648
Patorn Utenpattanun <patorn.u@gmail.com>
2013-06-04 18:17:30 +0700

    FIX create paysbuy transaction

--- app/models/spree/paysbuy_transaction.rb
+++ app/models/spree/paysbuy_transaction.rb
@@ -1,17 +1,18 @@
 module Spree
   class PaysbuyTransaction < ActiveRecord::Base
     has_many :payments, :as => :source
+    attr_accessible :ap_code, :result, :amount, :fee, :method, :confirm_cs
 
     def actions
       []
     end
 
     def self.create_from_postback(params)
-       PaysbuyTransaction.create(:ap_code => params[:apcode],
+       PaysbuyTransaction.create(:ap_code => params[:apCode],
                                :result => params[:result],
                                :amount => params[:amt],
                                :fee => params[:fee],
-                               :payment_method => params[:method],
+                               :method => params[:method],
                                :confirm_cs => params[:confirm_cs]
                               )
     end
